d307ca37bbaaee8791ca6856fb461599d456fde4
Murat Siyfiyev <46980962+Murat-Siyfiyev@users.noreply.github.com>
2019-03-01 23:32:09 -0700

    hmwk 2
    hmwk 2

---
+++ auth.js
@@ -0,0 +1,19 @@
+// Load required packages
+var passport = require('passport');
+var BasicStrategy = require('passport-http').BasicStrategy;
+
+passport.use(new BasicStrategy(
+    function(username, password, done) {
+        //hard coded
+        var user = { name: "Murat" };
+        if (username == user.name && password == "12345")
+        {
+            return done(null, user);
+        }
+        else
+        {
+            return done(null, false);
+        }
+    }
+));
+exports.isAuthenticated = passport.authenticate('basic', { session : false });
